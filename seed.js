6ed1ce2ba0c8580d949252f681fc625c1064ab9a
ryanthomas92 <rthoma11@villanova.edu>
2016-12-16 15:17:14 -0800

    completed seed data

--- seed.js
+++ seed.js
@@ -1,23 +1,16 @@
 var db = require('./models');
 
-var project_list = [
+var projects_list = [
   {
-  name: "Project 0",
-  description: "First Project"
+  projectName: "Project 0",
+  description: "First Project",
+  githubRepoUrl: "https://github.com/ryanthomas92/project-0"
   }
 ]
 
-db.Project.remove({}, function(err, projects) {
-  if (err) {
-    console.log('Error occurred in remove', err);
-  } else {
-    console.log("Removed all projects");
-
-    db.Project.create(projects_list, function(err, projects){
-      if (err){ return console.log("Error:", err); }
-
-      console.log("Created ", projects.length, " projects");
-      process.exit(); // we're all done! Exit the program.
-    });
-  }
+db.Project.remove({}, function(err, queryResponse) {
+  db.Project.create(projects_list, function(err, createdProject) {
+    console.log("Created " + createdProject.length + " projects");
+    process.exit();
+  });
 });
