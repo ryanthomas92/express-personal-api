c4d3ee3de6aa3b454369dfa588c4af23ee76845a
ryanthomas92 <rthoma11@villanova.edu>
2016-12-15 18:13:19 -0800

    gave seed data

--- seed.js
+++ seed.js
@@ -1,9 +1,6 @@
-This file allows us to seed our application with data
-simply run: `node seed.js` from the root of this project folder.
-
 var db = require('./models');
 
-var new_project = {name: "Project 0", description: "First Project"}
+var new_project = {name: "Project 0", description: "First Project"};
 
 db.Project.create(new_project, function(err, project){
   if (err){
@@ -12,4 +9,4 @@ db.Project.create(new_project, function(err, project){
 
   console.log("Created new campsite", project._id)
   process.exit(); // we're all done! Exit the program.
-})
+});
