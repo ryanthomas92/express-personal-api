e9ad35e9ce2095631b8cdf5eed798f11d3b68b9d
ryanthomas92 <rthoma11@villanova.edu>
2016-12-16 16:18:58 -0800

    finalized connections in index

--- models/index.js
+++ models/index.js
@@ -1,4 +1,8 @@
+// require mongoose
 var mongoose = require("mongoose");
+
+// connect to database
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
 
+// requires then exports the projects model
 module.exports.Project = require("./projects.js");
