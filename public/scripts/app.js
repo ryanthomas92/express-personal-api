def2d7a8b8b0d057a483047c3d9a2abefd489aa6
ryanthomas92 <rthoma11@villanova.edu>
2016-12-18 20:51:28 -0800

    wrote functions for handlebars

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -39,3 +39,53 @@ $(document).ready(function(){
   });
 
 });
+
+function render () {
+  $projectsList.empty();
+
+  var projectHtml;
+
+  allProjects.forEach(function(projectData) {
+    projectHtml = template({project: projectData});
+
+    $projectsList.append(projectHtml);
+  });
+};
+
+function onSuccess(json) {
+  allProjects = json;
+  render();
+}
+
+function onError(json) {
+  console.log('Error');
+  $('#projectTarget').text('Failed to load projects');
+}
+
+function newProjectSuccess(json) {
+  $('#newProjectForm input').val('');
+  allProjects.push(json);
+  render();
+}
+
+function newProjectError() {
+  console.log("New Project Error");
+}
+
+function deleteProjectSuccess(json) {
+  var project = json;
+  console.log('deleted project ', project);
+  var projectId = project._id;
+
+  for(var i = 0; i < allProjects.length; i++) {
+    if(allProjects[i]._id === projectId) {
+      allProjects.splice(i, 1);
+      break;
+    }
+  }
+  render();
+}
+
+function deleteProjectError() {
+  console.log('Delete Project Error');
+}
