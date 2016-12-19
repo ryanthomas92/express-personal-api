bcef0412ca0520f4e98b4a01d55b3279485f1526
ryanthomas92 <rthoma11@villanova.edu>
2016-12-18 20:25:14 -0800

    set up ajax routes and templates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,41 @@
 console.log("Sanity Check: JS is working!");
 
-$(document).ready(function(){
+var template;
+var $projectList;
+var allProjects = [];
 
-// your code
+$(document).ready(function(){
+  $projectList = $('#projectTarget');
+
+  var source = $('project-template').html();
+  template = Handlebars.compile(source);
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/projects',
+    success: onSuccess,
+    error: onError
+  });
+
+  $("#newProjectForm").on("submit", function(el) {
+    el.preventDefault();
+    $.ajax({
+      method: 'POST',
+      url: '/api/projects',
+      data: $(this).serialize(),
+      success: newProjectSuccess,
+      error: newProjectError
+    });
+  });
+
+  $projectsList.on("click", ".deleteBtn", function() {
+    console.log("clicked delete button to ", '/api/projects' + $(this).attr('data-id'));
+    $.ajax({
+      method: 'DELETE',
+      url: '/api/projects/' + $(this).attr('data-id'),
+      success: deleteProjectSuccess,
+      error: deleteProjectError
+    });
+  });
 
 });
